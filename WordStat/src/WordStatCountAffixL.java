1d910d2c9a3f3c326017d2b3ec1d353296f710e7
Matrixoid <mr.art.pro@mail.ru>
2023-11-20 23:47:34 +0300

    Update HW4 with new tests 2023

---
+++ WordStat/src/WordStatCountAffixL.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordStatCountAffixL {
+    public static void main(String[] args) {
+        String inputFileName = "", outputFileName = "";
+        try {
+            inputFileName = args[0];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Отсутствует имя входного файла");
+            return;
+        }
+        try {
+            outputFileName = args[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Отсутствует имя выходного файла");
+            return;
+        }
+        Map<String, Integer> result = new LinkedHashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFileName), StandardCharsets.UTF_8));
+            while (true) {
+                String str = reader.readLine();
+                if (str == null)
+                    break;
+                StringBuilder sb = new StringBuilder(str);
+                sb.append(" ");
+                StringBuilder word = new StringBuilder();
+                for (int i = 0; i < sb.length(); i++) {
+                    if (Character.isLetter(sb.charAt(i)) || sb.charAt(i) == '\'' || Character.getType(sb.charAt(i)) == Character.DASH_PUNCTUATION) {
+                        word.append(sb.charAt(i));
+                    } else {
+                        String s = word.toString().toLowerCase();
+                        if (!word.isEmpty() && word.length() >= 2) {
+                            String sPrefix = s.substring(0, 2);
+                            String sSuffix = s.substring(s.length() - 2);
+                            if (!result.containsKey(sPrefix)) {
+                                result.put(sPrefix, 1);
+                            } else {
+                                result.put(sPrefix, result.get(sPrefix) + 1);
+                            }
+                            if (!result.containsKey(sSuffix)) {
+                                result.put(sSuffix, 1);
+                            } else {
+                                result.put(sSuffix, result.get(sSuffix) + 1);
+                            }
+                        }
+                        word = new StringBuilder();
+                    }
+                }
+            }
+            reader.close();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFileName), StandardCharsets.UTF_8));
+            List<Map.Entry<String, Integer>> entries = new ArrayList<>(result.entrySet());
+            entries.sort(Map.Entry.comparingByValue());
+            for (Map.Entry<String, Integer> p : entries) {
+                writer.write(p.getKey() + " " + p.getValue() + "\n");
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не был найден");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- WordStat/tests/run.bat
+++ WordStat/tests/run.bat
@@ -1,8 +1,15 @@
-set pth=%cd%\WordStatTest.jar
-cd ..\out\production\WordStat
+set pth=%cd%\WordStatTest2022.jar
+set pth2=%cd%\WordStatTest2023.jar
+cd ../src
+javac *.java
 java -ea -jar %pth% Base
 java -ea -jar %pth% Words
 java -ea -jar %pth% WordsSuffix
 java -ea -jar %pth% WordsPrefix
 java -ea -jar %pth% WordsShingles
+java -ea -jar %pth2% CountPrefixL
+java -ea -jar %pth2% Count
+java -ea -jar %pth2% CountMiddleL
+java -ea -jar %pth2% CountAffixL
+del /s /q *.class
 pause
